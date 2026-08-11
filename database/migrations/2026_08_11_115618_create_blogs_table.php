ff1267f24e1587c9843a8f3b3de8ec946ca6e837
Saumy Anand <samcool3203@gmail.com>
2026-08-11 17:30:02 +0530

    feat: initialize blog system models and database migrations for categories, blogs, and images

---
+++ database/migrations/2026_08_11_115618_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('category_id')->constrained('blog_categories')->onDelete('cascade');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->string('image');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
